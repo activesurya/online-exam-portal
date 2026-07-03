80f5f850ee1fd38f491f952769904a251907c4d1
activesurya <activesurya@gmail.com>
2026-07-03 11:04:38 +0530

    Add Spring Boot main application class

---
+++ backend/src/main/java/com/examination/ExamPortalApplication.java
@@ -0,0 +1,19 @@
+package com.examination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
+
+@SpringBootApplication
+public class ExamPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPortalApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
